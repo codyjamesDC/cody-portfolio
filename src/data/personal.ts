2d10bafdbd1020222b1a534cb344eb0340b1a46b
codyjamesDC <cbdelacruz10@up.edu.ph>
2026-07-12 18:46:17 +0800

    chore: update tagline

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -1,6 +1,6 @@
 export const personal = {
   name: 'Cody James Dela Cruz',
-  tagline: 'UI/UX Lead & Full-Stack Developer',
+  tagline: 'UI/UX Designer & Full-Stack Developer',
   subTagline: '3rd-year CS Student @ University of the Philippines Los Baños',
   bio: 'A goal-driven computer science student with strong communication, openness to feedback, and adaptability developed through prior leadership experiences. Seeking to expand opportunities and contribute to organizational growth, as well as develop one\'s own skills beyond the academe.',
   email: 'codyjamesberchesdelacruz@gmail.com',
